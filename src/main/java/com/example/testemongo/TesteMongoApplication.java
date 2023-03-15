e495a761658210a2ab23ca0ab3c832331cdb63da
vowkaz <jlucasgarmier@gmail.com>
2023-03-15 17:43:04 -0400

    first commit

---
+++ src/main/java/com/example/testemongo/TesteMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteMongoApplication.class, args);
+    }
+
+}
